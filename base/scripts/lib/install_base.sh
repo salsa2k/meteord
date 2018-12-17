16ef22ba33daa26a91a399780630cdf5ed435c92
Rafael Noronha <rafinha@rafinha.org>
2018-12-17 16:43:39 -0500

    fix error

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 apt-get update -y
-apt-get install -y curl bzip2 build-essential g++ python git build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev libjpeg8-dev
+apt-get install -y curl bzip2 build-essential g++ python git build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
